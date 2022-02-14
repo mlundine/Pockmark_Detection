0b0420f42232a23e24dc0f642506bca5fc37b977
unknown <mlundine@udel.edu>
2022-02-14 13:23:07 -0500

    added main file

---
+++ pockmark_detection_main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import glob
+
+
+
+def tile_grid(path_to_input,
+              path_to_output,
+              tile_size_x,
+              tile_size_y,
+              overlap):
+    """
+    Takes a large bathymetry grid and slices it into square tiles
+    """
+
+def convert_tif_to_jpeg(tiff_folder,
+                        jpeg_folder):
+    """
+    Converts geotiffs to jpegs
+    """
+
+def run_detection_model(model,
+                        input_path,
+                        output_path):
+    """
+    Runs a trained pockmark detector
+    """
+
+def process_detection_results(input_path,
+                              output_path,
+                              threshold):
+    """
+    Outputs GIS files for pockmark detection results
+    """
